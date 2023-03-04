b3c7bceef178bf203741d20c0db3a43f6704138b
Netra Krishnan <netra.kris@gmail.com>
2023-03-04 13:46:47 -0500

    first

---
+++ genderequalityapp/src/pages/home.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './home.css';
+
+function Home() {
+
+  return (
+    <div className='Home'>
+     jdbfldskjbvdkjzvnkxdn
+
+    </div>
+  )
+}
+
+export default Home;
