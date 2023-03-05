7781ab60cf921b2ebeaa44bf5261c8c7cf15f73a
abritidas <69867532+abritidas@users.noreply.github.com>
2023-03-04 20:41:40 -0500

    Update home.jsx

--- genderequalityapp/src/pages/home.jsx
+++ genderequalityapp/src/pages/home.jsx
@@ -1,15 +1,29 @@
 
 import React from 'react';
-import './home.css';
+import { makeStyles } from '@material-ui/core/styles';
+import TopMenu from '../components/TopMenu';
+import SideMenu from '../components/SideMenu';
+import Footer from '../components/Footer';
+import MainContent from '../components/MainContent';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
 
   return (
-    <div className='Home'>
-     jdbfldskjbvdkjzvnkxdn
-
-    </div>
-  )
+    <div className={classes.root}>
+    
+    <TopMenu />
+    <SideMenu />
+    <MainContent />
+    <Footer />
+  </div>
+);
 }
 
 export default Home;
