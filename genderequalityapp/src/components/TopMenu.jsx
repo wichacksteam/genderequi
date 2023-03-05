e1bf83955f168255c823b6a47dd2af29aad3686a
Abriti Das <abritidas89@gmail.com>
2023-03-04 21:38:14 -0500

    intial page changes

--- genderequalityapp/src/components/TopMenu.jsx
+++ genderequalityapp/src/components/TopMenu.jsx
@@ -28,7 +28,14 @@ function TopMenu() {
   return (
     <AppBar position='fixed' className={classes.appBar}>
       <Toolbar>
-    
+        {/* <IconButton
+          edge='start'
+          className={classes.menuButton}
+          color='inherit'
+          aria-label='menu'
+        >
+          <MenuIcon />
+        </IconButton> */}
         <MenuItem>
           <Typography variant='h6' className={classes.title}>
             Home
